a88514ef4424b16a8a7b1e7ba67a998afc9d60d9
SanjeevSaniel <sanjeevsaniel@outlook.in>
2022-11-13 14:18:11 +0530

    CORS

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const path = require("path");
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 const app = express();
 
-app.use(express.static(path.join(__dirname, "..", "build")));
 // app.use(cors);
+app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.static("public"));
-
 
 app.get("/", (req, res) => {
   res.send("This is from express.js");
+});
+
+app.get("/api/Bollywood.json", cors(), (req, res) => {
+  res.status(200);
 });
 
 // app.use((req, res, next) => {
@@ -17,7 +20,7 @@ app.get("/", (req, res) => {
 // });
 
 // start express server on port 3000
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`server started on http://localhost:${port}`);
 });
